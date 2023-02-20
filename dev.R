9d524dd6450d6f01fb4a75c20613d90c63a0e4d4
benyamindsmith <benjamin@alterclimate.com>
2023-02-20 12:43:12 -0500

    progress

--- dev.R
+++ dev.R
@@ -1,6 +1,6 @@
 # Chess Board in R
 
-library(tidyverse)
+library(ggplot2)
 
 cols <- letters[1:8]
 rows <- 1:8
@@ -13,92 +13,115 @@ board <- expand.grid(cols,rows) |>
                    FALSE)
   })
 
+# For filtering
 pieces <- {
   list(
     white_pawns = board |>
       subset(Var2 == 2) |>
       within({
+        piece = "white_pawn"
         no = 1:8
         alive = TRUE
+        check=NA
         position = list(c("a",2), c("b",2),c("c",2),c("d",2),c("e",2),c("f",2),c("g",2),c("h",2))
         turn_count = 0
       }),
     black_pawns = board |>
       subset(Var2 == 7) |>
       within({
+        piece = "black_pawn"
         no = 1:8
         alive = TRUE
+        check=NA
         position = list(c("a",7), c("b",7),c("c",7),c("d",7),c("e",7),c("f",7),c("g",7),c("h",7))
         turn_count = 0
       }),
     white_castles = board |>
       subset(Var2 == 1 & Var1 %in% letters[c(1, 8)]) |>
       within({
+        piece = "white_castle"
         no = 1:2
         alive = TRUE
+        check=NA
         position = list(c("a",1), c("h",1))
         turn_count = 0
       }),
     black_castles = board |>
       subset(Var2 == 8 & Var1 %in% letters[c(1, 8)]) |>
       within({
+        piece = "black_castle"
         no = 1:2
         alive = TRUE
+        check=NA
         position = list(c("a",8), c("h",8))
         turn_count = 0
       }),
     white_knights = board |>
       subset(Var2 == 1 & Var1 %in% letters[c(2, 7)]) |>
       within({
+        piece = "white_knights"
         no = 1:2
         alive = TRUE
+        check=NA
         position = list(c("b",1), c("g",1))
         turn_count =0
       }),
     black_knights = board |>
       subset(Var2 == 8 & Var1 %in% letters[c(2, 7)]) |>
       within({
+        piece = "black_knights"
         no = 1:2
         alive = TRUE
+        check=NA
         position = list(c("b",8), c("g",8))
         turn_count =0
       }),
     white_bishops = board |>
       subset(Var2 == 1 & Var1 %in% letters[c(3, 6)]) |>
       within({
+        piece="white_bishop"
         no = 1:2
         alive = TRUE
+        check=NA
         position = list(c("c",1), c("f",1))
         turn_count =0
       }),
     black_bishops = board |>
       subset(Var2 == 8 & Var1 %in% letters[c(3, 6)]) |>
       within({
+        piece = "black_bishop"
         no = 1:2
         alive=TRUE
+        check=NA
         position = list(c("c",8), c("f",8))
         turn_count=0
       }),
     white_queen = board |> 
       subset(Var2 == 1 & Var1 == letters[4])|>
       within({
+        piece="white_queen"
         no = 1
         alive=TRUE
+        check=NA
         position = list(c("d",1))
         turn_count=0
       }),
     black_queen = board |> 
       subset(Var2 == 8 & Var1 == letters[4]) |>
       within({
+        piece="black_queen"
         no = 1
         alive=TRUE
+        check=NA
         position = list(c("d",8))
         turn_count=0
       }),
     white_king = board |> 
       subset(Var2 == 1 & Var1 == letters[5]) |>
       within({
+        piece = "white_king"
         no = 1
+        alive=TRUE
         check=FALSE
         position = list(c("e",1))
         turn_count=0
@@ -106,18 +129,33 @@ pieces <- {
     black_king = board |> 
       subset(Var2 == 8 & Var1 == letters[5]) |>
       within({
+        piece="black_king"
         no = 1
         position = list(c("d",8))
+        alive=TRUE
         check=FALSE
         turn_count=0
       })
   )
 }
 
+# For reference
+pieces_dataset <- do.call(rbind, pieces)
+
+# white pieces
+
+white_pieces <- ""
+
+# black pieces
+
+
+white_pieces <- ""
+
 validate_move_syntax <- function(move){
   check<-grepl("(^((?:[KQRBN])|)[a-h][1-8]$)|(quit)",move)
   return(check)
 }
+
 
 
 # STILL NEED TO WORK ON
@@ -232,11 +270,15 @@ check_move_legality<- function(player,parsed_move){
     }
 }
 
+
 get_player_move <- function(player){
   prompt <- paste(player, "'s Turn:", sep='', collapse='')
   
   move<-readline(prompt)
   
+  if(move=="quit"){
+    return(move)
+  }else{
   # Recursive solution
   
   # Check for valid move
@@ -251,8 +293,8 @@ get_player_move <- function(player){
     cat("Invalid Move. Please use algebraic notation for making moves.")
     get_player_move(player)
   }
+  }
 }
-
 
 plot_board<- function(board,
                       pieces){

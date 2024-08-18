65f2ecc37ca3c9b6d5eefdb0bd046a56a7766a92
benyamindsmith <benjamin@alterclimate.com>
2024-08-18 17:49:11 -0400

    gameplay somewhat works

--- R/new_game.R
+++ R/new_game.R
@@ -7,6 +7,24 @@ new_game <- function() {
   screen_height <- 800
   board_size <- 8
   square_size <- screen_width / board_size
+  selected_piece <- NULL
+  selected_pos <- NULL
+
+  # Board matrix to keep track of piece positions
+  board <- matrix("", nrow = 8, ncol = 8)
+  board[2, ] <- "white_pawn"
+  board[7, ] <- "black_pawn"
+  board[1, c(1, 8)] <- "white_rook"
+  board[1, c(2, 7)] <- "white_knight"
+  board[1, c(3, 6)] <- "white_bishop"
+  board[1, 4] <- "white_queen"
+  board[1, 5] <- "white_king"
+  board[8, c(1, 8)] <- "black_rook"
+  board[8, c(2, 7)] <- "black_knight"
+  board[8, c(3, 6)] <- "black_bishop"
+  board[8, 4] <- "black_queen"
+  board[8, 5] <- "black_king"
+
 
   # Pieces
   pieces <- paste0(
@@ -20,107 +38,92 @@ new_game <- function() {
   raylibr::init_window(width = screen_width,
                        height = screen_height,
                        title = "rChess")
-  # Define pieces
 
-  # # Black
-  # black_bishop <- pieces[[1]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  # black_king <- pieces[[2]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  # black_knight <- pieces[[3]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  black_pawn <- pieces[[4]] |>
-    raylibr::load_image() |>
-    raylibr::load_texture_from_image()
-  # black_queen <- pieces[[5]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  black_rook <- pieces[[6]] |>
-    raylibr::load_image() |>
-    raylibr::load_texture_from_image()
-  #
-  # # White
-  # white_bishop <- pieces[[7]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  # white_king <- pieces[[8]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  # white_knight <- pieces[[9]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  white_pawn <- pieces[[10]] |>
-    raylibr::load_image() |>
-    raylibr::load_texture_from_image()
-  # white_queen <- pieces[[11]] |>
-  #   raylibr::load_image() |>
-  #   raylibr::load_texture_from_image()
-  white_rook <- pieces[[12]] |>
-    raylibr::load_image() |>
-    raylibr::load_texture_from_image()
-
+  # Load pieces as textures
+  piece_textures <- list(
+    white_bishop = raylibr::load_image(pieces[[7]]) |> raylibr::load_texture_from_image(),
+    white_king = raylibr::load_image(pieces[[8]]) |> raylibr::load_texture_from_image(),
+    white_knight = raylibr::load_image(pieces[[9]]) |> raylibr::load_texture_from_image(),
+    white_pawn = raylibr::load_image(pieces[[10]]) |> raylibr::load_texture_from_image(),
+    white_queen = raylibr::load_image(pieces[[11]]) |> raylibr::load_texture_from_image(),
+    white_rook = raylibr::load_image(pieces[[12]]) |> raylibr::load_texture_from_image(),
+    black_bishop = raylibr::load_image(pieces[[1]]) |> raylibr::load_texture_from_image(),
+    black_king = raylibr::load_image(pieces[[2]]) |> raylibr::load_texture_from_image(),
+    black_knight = raylibr::load_image(pieces[[3]]) |> raylibr::load_texture_from_image(),
+    black_pawn = raylibr::load_image(pieces[[4]]) |> raylibr::load_texture_from_image(),
+    black_queen = raylibr::load_image(pieces[[5]]) |> raylibr::load_texture_from_image(),
+    black_rook = raylibr::load_image(pieces[[6]]) |> raylibr::load_texture_from_image()
+  )
 
   while (!raylibr::window_should_close()) {
     raylibr::begin_drawing()
-    # Setup Board
+
+    # Find the positions of the kings
+    white_king_pos <- which(board == "white_king", arr.ind = TRUE)[1, ]
+    black_king_pos <- which(board == "black_king", arr.ind = TRUE)[1, ]
+
+    # Determine if kings are in check
+    white_king_in_check <- rChess:::is_king_in_check(board, white_king_pos, "black")
+    black_king_in_check <- rChess:::is_king_in_check(board, black_king_pos, "white")
+
+    # Determine if kings are in checkmate (simplified)
+    white_king_in_checkmate <- white_king_in_check && rChess:::is_checkmate(board, white_king_pos, "white", "black")
+    black_king_in_checkmate <- black_king_in_check && rChess:::is_checkmate(board, black_king_pos, "black", "white")
+
+    # Draw the board
     for (row in 0:(board_size - 1)) {
       for (col in 0:(board_size - 1)) {
         if ((row + col) %% 2 == 0) {
-          raylibr::draw_rectangle(
-            col * square_size,
-            row * square_size,
-            square_size,
-            square_size,
-            col =  raylibr::color(240, 217, 181, 255)
-          )
+          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(240, 217, 181, 255))
         } else {
-          raylibr::draw_rectangle(
-            col * square_size,
-            row * square_size,
-            square_size,
-            square_size,
-            col = raylibr::color(181, 136, 99, 255)
-          )
+          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(181, 136, 99, 255))
+        }
+
+        # Highlight the selected square
+        if (!is.null(selected_pos) && selected_pos[1] == 8 - row && selected_pos[2] == col + 1) {
+          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(0, 255, 0, 100))  # Transparent green
+        }
+
+        # Highlight the king's square in red if in check
+        if (white_king_in_check && white_king_pos[1] == 8 - row && white_king_pos[2] == col + 1) {
+          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(255, 0, 0, 100))  # Transparent red
+        } else if (black_king_in_check && black_king_pos[1] == 8 - row && black_king_pos[2] == col + 1) {
+          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(255, 0, 0, 100))  # Transparent red
+        }
+
+        # Draw the piece at the current position
+        piece <- board[8 - row, col + 1]
+        if (piece != "") {
+          raylibr::draw_texture(piece_textures[[piece]], col * square_size - 15, row * square_size - 20, tint = raylibr::as_color("white"))
         }
       }
-      #Add pieces
-      #Draw Pawns
-      for (col in 0:(board_size - 1)) {
-        # White pawns
-        raylibr::draw_texture(white_pawn, col * square_size-15, 6 * square_size-20, tint = raylibr::as_color("white"))
-        # Black pawns
-        raylibr::draw_texture(black_pawn, col * square_size -15, 1 * square_size-20, tint = raylibr::as_color("white"))
-      }
-
-      # # Draw other pieces: Black
-      #raylibr::draw_texture(black_rook, 0 * square_size-15, 0 * square_size-20, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_knight, 1 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_bishop, 2 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_queen, 3 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_king, 4 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_bishop, 5 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_knight, 6 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(black_rook, 7 * square_size, 0 * square_size, tint = raylibr::color("white"))
-      #
-      # # Draw other pieces: White
-      #raylibr::draw_texture(white_rook, 0 * square_size, 6 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_knight, 1 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_bishop, 2 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_queen, 3 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_king, 4 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_bishop, 5 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_knight, 6 * square_size, 7 * square_size, tint = raylibr::color("white"))
-      # raylibr::draw_texture(white_rook, 7 * square_size, 7 * square_size, tint = raylibr::color("white"))
-
     }
+
+    # Detect clicks
+    if (raylibr::is_mouse_button_pressed(raylibr::mouse_button$left)) {
+      mouse_pos <- raylibr::get_mouse_position()
+      col <- floor(mouse_pos[1]/ square_size) + 1
+      row <- board_size - floor(mouse_pos[2] / square_size)
+
+      if (is.null(selected_piece)) {
+        # Selecting a piece
+        selected_piece <- board[row, col]
+        selected_pos <- c(row, col)
+      } else {
+        # Moving the selected piece
+        if (selected_piece != "" && rChess:::is_valid_move(board, selected_piece, selected_pos, c(row, col))) {
+          board[row, col] <- selected_piece
+          board[selected_pos[1], selected_pos[2]] <- ""
+        }
+        selected_piece <- NULL
+        selected_pos <- NULL
+      }
+    }
+    print(board)
     raylibr::end_drawing()
   }
 
   raylibr::close_window()
-
 }
 
-new_game()
+#new_game()
---
+++ R/utils.R
@@ -0,0 +1,142 @@
+is_valid_move <- function(board, piece, start_pos, end_pos) {
+  start_row <- start_pos[1]
+  start_col <- start_pos[2]
+  end_row <- end_pos[1]
+  end_col <- end_pos[2]
+
+  delta_row <- abs(end_row - start_row)
+  delta_col <- abs(end_col - start_col)
+
+  # Prevent moving to a square with a piece of the same color
+  if (startsWith(board[end_row, end_col], substr(piece, 1, 5))) {
+    return(FALSE)
+  }
+
+  if (piece == "white_pawn" || piece == "black_pawn") {
+    direction <- ifelse(piece == "white_pawn", 1, -1)
+    start_row_direction <- ifelse(piece == "white_pawn", 2, 7)
+
+    if (start_col == end_col) {
+      # Normal forward move
+      if (delta_row == 1 && (end_row - start_row) == direction && board[end_row, end_col] == "") {
+        return(TRUE)
+      }
+      # First move: two squares forward
+      if (start_row == start_row_direction && delta_row == 2 && (end_row - start_row) == direction * 2 && board[end_row, end_col] == "" && board[start_row + direction, end_col] == "") {
+        return(TRUE)
+      }
+    } else if (delta_row == 1 && delta_col == 1) {
+      # Capture
+      if (board[end_row, end_col] != "" && !startsWith(board[end_row, end_col], substr(piece, 1, 5))) {
+        return(TRUE)
+      }
+    }
+
+  } else if (piece == "white_rook" || piece == "black_rook") {
+    if (delta_row == 0 || delta_col == 0) {
+      # Check for clear path
+      if (delta_row == 0) {
+        step <- sign(end_col - start_col)
+        for (c in seq(start_col + step, end_col - step, step)) {
+          if (board[start_row, c] != "") {
+            return(FALSE)
+          }
+        }
+      } else if (delta_col == 0) {
+        step <- sign(end_row - start_row)
+        for (r in seq(start_row + step, end_row - step, step)) {
+          if (board[r, start_col] != "") {
+            return(FALSE)
+          }
+        }
+      }
+      return(TRUE)
+    }
+
+  } else if (piece == "white_knight" || piece == "black_knight") {
+    if ((delta_row == 2 && delta_col == 1) || (delta_row == 1 && delta_col == 2)) {
+      return(TRUE)
+    }
+
+  } else if (piece == "white_bishop" || piece == "black_bishop") {
+    if (delta_row == delta_col) {
+      # Check for clear path
+      row_step <- sign(end_row - start_row)
+      col_step <- sign(end_col - start_col)
+      for (i in 1:(delta_row - 1)) {
+        if (board[start_row + i * row_step, start_col + i * col_step] != "") {
+          return(FALSE)
+        }
+      }
+      return(TRUE)
+    }
+
+  } else if (piece == "white_queen" || piece == "black_queen") {
+    if (delta_row == delta_col || delta_row == 0 || delta_col == 0) {
+      # Check for clear path (combine rook and bishop logic)
+      if (delta_row == delta_col) {
+        row_step <- sign(end_row - start_row)
+        col_step <- sign(end_col - start_col)
+        for (i in 1:(delta_row - 1)) {
+          if (board[start_row + i * row_step, start_col + i * col_step] != "") {
+            return(FALSE)
+          }
+        }
+      } else if (delta_row == 0) {
+        step <- sign(end_col - start_col)
+        for (c in seq(start_col + step, end_col - step, step)) {
+          if (board[start_row, c] != "") {
+            return(FALSE)
+          }
+        }
+      } else if (delta_col == 0) {
+        step <- sign(end_row - start_row)
+        for (r in seq(start_row + step, end_row - step, step)) {
+          if (board[r, start_col] != "") {
+            return(FALSE)
+          }
+        }
+      }
+      return(TRUE)
+    }
+
+  } else if (piece == "white_king" || piece == "black_king") {
+    if (delta_row <= 1 && delta_col <= 1) {
+      return(TRUE)
+    }
+  }
+
+  return(FALSE)
+}
+
+is_king_in_check <- function(board, king_position, opponent_color) {
+  for (row in 1:8) {
+    for (col in 1:8) {
+      piece <- board[row, col]
+      if (startsWith(piece, opponent_color)) {
+        if (is_valid_move(board, piece, c(row, col), king_position)) {
+          return(TRUE)
+        }
+      }
+    }
+  }
+  return(FALSE)
+}
+
+is_checkmate <- function(board, king_position, king_color, opponent_color) {
+  # Check if the king can move to any adjacent square and escape check
+  directions <- list(
+    c(-1, 0), c(1, 0), c(0, -1), c(0, 1), c(-1, -1), c(-1, 1), c(1, -1), c(1, 1)
+  )
+
+  for (direction in directions) {
+    new_row <- king_position[1] + direction[1]
+    new_col <- king_position[2] + direction[2]
+    if (new_row >= 1 && new_row <= 8 && new_col >= 1 && new_col <= 8) {
+      if (!is_king_in_check(board, c(new_row, new_col), opponent_color)) {
+        return(FALSE)
+      }
+    }
+  }
+}
+

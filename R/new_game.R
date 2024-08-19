96f0637f8a4b3ef79a7f4cd3c4c5b8333e641868
benyamindsmith <benjamin@alterclimate.com>
2024-08-18 21:21:31 -0400

    adding logo

--- R/new_game.R
+++ R/new_game.R
@@ -2,6 +2,7 @@
 #'
 #' @export
 
+
 new_game <- function() {
   screen_width <- 800
   screen_height <- 800
@@ -9,6 +10,7 @@ new_game <- function() {
   square_size <- screen_width / board_size
   selected_piece <- NULL
   selected_pos <- NULL
+  valid_moves <- list()
 
   # Board matrix to keep track of piece positions
   board <- matrix("", nrow = 8, ncol = 8)
@@ -25,23 +27,19 @@ new_game <- function() {
   board[8, 4] <- "black_queen"
   board[8, 5] <- "black_king"
 
-
   # Pieces
-  pieces <- paste0(
-    paste0(system.file(package="rChess"),"/png/"),
-    paste0(system.file(package="rChess"),"/png") |>
-    list.files()
-    )
-
+  pieces <- paste0(paste0(system.file(package = "rChess"), "/png/"),
+                   paste0(system.file(package = "rChess"), "/png") |>
+                     list.files())
 
   # Define Window
   raylibr::init_window(width = screen_width,
                        height = screen_height,
                        title = "rChess")
-
+  # Add window icon
+  #raylibr::set_window_icon()
   # Load pieces as textures
-  piece_textures <- list(
-    white_bishop = raylibr::load_image(pieces[[7]]) |> raylibr::load_texture_from_image(),
+  piece_textures <- list(ure_from_image(),
     white_king = raylibr::load_image(pieces[[8]]) |> raylibr::load_texture_from_image(),
     white_knight = raylibr::load_image(pieces[[9]]) |> raylibr::load_texture_from_image(),
     white_pawn = raylibr::load_image(pieces[[10]]) |> raylibr::load_texture_from_image(),
@@ -67,34 +65,87 @@ new_game <- function() {
     black_king_in_check <- rChess:::is_king_in_check(board, black_king_pos, "white")
 
     # Determine if kings are in checkmate (simplified)
-    white_king_in_checkmate <- white_king_in_check && rChess:::is_checkmate(board, white_king_pos, "white", "black")
-    black_king_in_checkmate <- black_king_in_check && rChess:::is_checkmate(board, black_king_pos, "black", "white")
+    white_king_in_checkmate <- white_king_in_check &&
+      rChess:::is_checkmate(board, white_king_pos, "white", "black")
+    black_king_in_checkmate <- black_king_in_check &&
+      rChess:::is_checkmate(board, black_king_pos, "black", "white")
 
     # Draw the board
     for (row in 0:(board_size - 1)) {
       for (col in 0:(board_size - 1)) {
         if ((row + col) %% 2 == 0) {
-          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(240, 217, 181, 255))
+          raylibr::draw_rectangle(
+            col * square_size,
+            row * square_size,
+            square_size,
+            square_size,
+            raylibr::color(240, 217, 181, 255)
+          )
         } else {
-          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(181, 136, 99, 255))
+          raylibr::draw_rectangle(
+            col * square_size,
+            row * square_size,
+            square_size,
+            square_size,
+            raylibr::color(181, 136, 99, 255)
+          )
         }
 
         # Highlight the selected square
-        if (!is.null(selected_pos) && selected_pos[1] == 8 - row && selected_pos[2] == col + 1) {
-          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(0, 255, 0, 100))  # Transparent green
+        if (!is.null(selected_pos) &&
+            selected_pos[1] == 8 - row && selected_pos[2] == col + 1) {
+          raylibr::draw_rectangle(
+            col * square_size,
+            row * square_size,
+            square_size,
+            square_size,
+            raylibr::color(0, 255, 0, 100)
+          )  # Transparent green
+        }
+
+        # Highlight the valid moves in yellow
+        for (move in valid_moves) {
+          if (move[1] == 8 - row && move[2] == col + 1) {
+            raylibr::draw_rectangle(
+              col * square_size,
+              row * square_size,
+              square_size,
+              square_size,
+              raylibr::color(255, 255, 0, 100)
+            )  # Transparent yellow
+          }
         }
 
         # Highlight the king's square in red if in check
-        if (white_king_in_check && white_king_pos[1] == 8 - row && white_king_pos[2] == col + 1) {
-          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(255, 0, 0, 100))  # Transparent red
-        } else if (black_king_in_check && black_king_pos[1] == 8 - row && black_king_pos[2] == col + 1) {
-          raylibr::draw_rectangle(col * square_size, row * square_size, square_size, square_size, raylibr::color(255, 0, 0, 100))  # Transparent red
+        if (white_king_in_check &&
+            white_king_pos[1] == 8 - row && white_king_pos[2] == col + 1) {
+          raylibr::draw_rectangle(
+            col * square_size,
+            row * square_size,
+            square_size,
+            square_size,
+            raylibr::color(255, 0, 0, 100)
+          )  # Transparent red
+        } else if (black_king_in_check &&
+                   black_king_pos[1] == 8 - row && black_king_pos[2] == col + 1) {
+          raylibr::draw_rectangle(
+            col * square_size,
+            row * square_size,
+            square_size,
+            square_size,
+            raylibr::color(255, 0, 0, 100)
+          )  # Transparent red
         }
 
         # Draw the piece at the current position
         piece <- board[8 - row, col + 1]
         if (piece != "") {
-          raylibr::draw_texture(piece_textures[[piece]], col * square_size - 15, row * square_size - 20, tint = raylibr::as_color("white"))
+          raylibr::draw_texture(
+            piece_textures[[piece]],
+            col * square_size - 15,
+            row * square_size - 20,
+            tint = raylibr::as_color("white")
+          )
         }
       }
     }
@@ -102,27 +153,40 @@ new_game <- function() {
     # Detect clicks
     if (raylibr::is_mouse_button_pressed(raylibr::mouse_button$left)) {
       mouse_pos <- raylibr::get_mouse_position()
-      col <- floor(mouse_pos[1]/ square_size) + 1
+      col <- floor(mouse_pos[1] / square_size) + 1
       row <- board_size - floor(mouse_pos[2] / square_size)
 
       if (is.null(selected_piece)) {
         # Selecting a piece
         selected_piece <- board[row, col]
         selected_pos <- c(row, col)
+        valid_moves <- list()
+
+        # Calculate valid moves
+        if (selected_piece != "") {
+          for (r in 1:8) {
+            for (c in 1:8) {
+              if (rChess:::is_valid_move(board, selected_piece, selected_pos, c(r, c))) {
+                valid_moves <- append(valid_moves, list(c(r, c)))
+              }
+            }
+          }
+        }
       } else {
         # Moving the selected piece
-        if (selected_piece != "" && rChess:::is_valid_move(board, selected_piece, selected_pos, c(row, col))) {
+        if (selected_piece != "" &&
+            rChess:::is_valid_move(board, selected_piece, selected_pos, c(row, col))) {
           board[row, col] <- selected_piece
           board[selected_pos[1], selected_pos[2]] <- ""
         }
         selected_piece <- NULL
         selected_pos <- NULL
+        valid_moves <- list()
       }
     }
+
     raylibr::end_drawing()
   }
 
   raylibr::close_window()
 }
-
-#new_game()
